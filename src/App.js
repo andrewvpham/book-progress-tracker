766f640421886ef5555b30ea5adaa277f2798d67
Andrew Pham <40774904+andrewvpham@users.noreply.github.com>
2022-05-11 21:13:01 -0700

    Added onClick drawer
    Title

--- src/App.js
+++ src/App.js
@@ -3,20 +3,67 @@ import './App.css';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { useState } from "react";
+import {
+  CheckBoxOutlineBlankOutlined,
+  DraftsOutlined,
+  HomeOutlined,
+  InboxOutlined,
+  MailOutline,
+  ReceiptOutlined,
+} from "@mui/icons-material";
+
+
+const data = [
+  {
+    name: "Home",
+    icon: <HomeOutlined />,
+  },
+  { name: "Inbox", icon: <InboxOutlined /> },
+  { name: "Outbox", icon: <CheckBoxOutlineBlankOutlined /> },
+  { name: "Sent mail", icon: <MailOutline /> },
+  { name: "Draft", icon: <DraftsOutlined /> },
+  { name: "Trash", icon: <ReceiptOutlined /> },
+];
+
+
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const styles = {
+    paper: {
+      background: "blue"
+    }
+  }
+
+  const getList = () => (
+    <div style={{ width: 250 }} onClick={() => setOpen(false)}>
+      {data.map((item, index) => (
+        <ListItem button key={index}>
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      
-      <AppBar position="static">
+      <Drawer open={open} anchor={"left"} onClose={() => setOpen(false)} >
+        {getList()}
+      </Drawer>
+      <AppBar position="relative">
       <Toolbar variant="dense">
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => setOpen(true)}>
           <MenuIcon />
         </IconButton>
         <Typography variant="h6" color="inherit" component="div">
